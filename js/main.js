4f0eb09ec44fda9f3d1c5d1baf8069bccabab209
zacchelynmejias <zacchelynmejias@gmail.com>
2023-10-14 21:32:26 +0800

    modify and adding validation

--- js/main.js
+++ js/main.js
@@ -1,26 +1,74 @@
-(function($) {
+function validateform(){
 
-	"use strict";
+	const username = document.getElementById("username").value;
+	const password = document.getElementById("password").value;
+	
+	const usernameRegex = /^[a-zA-Z0-9_]+$/;
+	const passwordRegex =  /^[A-Za-z]\w{7,14}$/;
 
-	var fullHeight = function() {
+	const isValid = true;
 
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
+	try {
+		if (!username.match(usernameRegex)) {
+			alert("Invalid username format. Please use only letters, numbers, and underscores.");
+			isValid = false;
+		} else {
+			document.getElementById("usernameError").innerHTML = "";
+		}
 
-	};
-	fullHeight();
+		// Validate password
+		if (!password.match(passwordRegex)) {
+			alert("Invalid format. Please use letters and numbers only.");
+			isValid = false;
+		} else {
+			document.getElementById("passwordError").innerHTML = "";
+		}
+		// console.log(z);
 
-	$(".toggle-password").click(function() {
+	} catch (error) {
+		alert(error.message);
+		isValid = false;
+		
+	} finally {
+	   }
 
-	  $(this).toggleClass("fa-eye fa-eye-slash");
-	  var input = $($(this).attr("toggle"));
-	  if (input.attr("type") == "password") {
-	    input.attr("type", "text");
-	  } else {
-	    input.attr("type", "password");
-	  }
-	});
+		if (isValid) {
+			window.location.href = "dashboard.html";
+		}
 
-})(jQuery);
+		return isValid;
+
+}  
+
+
+// (function($) {
+
+// 	"use strict";
+
+// 	var fullHeight = function() {
+
+// 		$('.js-fullheight').css('height', $(window).height());
+// 		$(window).resize(function(){
+// 			$('.js-fullheight').css('height', $(window).height());
+// 		});
+
+// 	};
+// 	fullHeight();
+
+
+// 	$(".toggle-password").click(function() {
+
+// 	  $(this).toggleClass("fa-eye fa-eye-slash");
+// 	  var input = $($(this).attr("toggle"));
+
+// 	  if (input.attr("type") == "password") {
+// 	    input.attr("type", "text");
+// 	  } else {
+// 	    input.attr("type", "password");
+// 	  }  
+// 	});
+
+
+
+// })(jsQuery);
+
