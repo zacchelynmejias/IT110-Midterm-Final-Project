eb20cfee94b7806a290b42a8d5d0e0fc99262f66
zacchelynmejias <zacchelynmejias@gmail.com>
2023-10-15 14:22:30 +0800

    Modify Dashboard

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,33 @@
 function myFunction(x) {
   x.classList.toggle("change");
 }
+
+
+  function toggleLogoutButton() {
+    var container = document.querySelector(".container");
+    var logoutButtonContainer = document.getElementById("logout-button-container");
+
+    // Toggle the "change" class for the container
+    container.classList.toggle("change");
+
+    if (!logoutButtonContainer.querySelector("button")) {
+        // Create the logout button
+        var logoutButton = document.createElement("button");
+        logoutButton.textContent = "Logout";
+        logoutButton.onclick = logout; // Set the logout function here
+
+        // Add the button to the container
+        logoutButtonContainer.appendChild(logoutButton);
+    } else {
+        // Remove the logout button if it already exists
+        logoutButtonContainer.removeChild(logoutButtonContainer.querySelector("button"));
+    }
+}
+
+function logout() {
+    // Add your logout logic here
+    window.location.href = "index.html";
+  }
+
+
+
