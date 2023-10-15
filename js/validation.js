521543134bd7e107d826f9988237b5f6141fdc4e
zacchelynmejias <zacchelynmejias@gmail.com>
2023-10-15 22:41:59 +0800

    modify validation alert

--- js/validation.js
+++ js/validation.js
@@ -3,13 +3,13 @@ function validateform(){
 	const password = document.getElementById("password").value;
 
 	const usernameRegex = /^[a-zA-Z0-9_]+$/;
-	const passwordRegex =  /^[A-Za-z]\w{7,14}$/;
+	const passwordRegex =  /^[A-Za-z_]\w{7,14}$/;
 
     const isValid = true;
 
     try {
 		if (!username.match(usernameRegex)) {
-			alert("Invalid username format. Please use only letters, numbers, and underscores.");
+			alert("Invalid username format. Please use letters, numbers, and underscores only.");
 			isValid = false;
 		} else {
 			document.getElementById("usernameError").innerHTML = "";
@@ -17,7 +17,7 @@ function validateform(){
 
         // Validate password
 		if (!password.match(passwordRegex)) {
-			alert("Invalid password format. Please enter 8 characters and use only letters and numbers only");
+			alert("Invalid password format. Please enter 8 characters and use letters, numbers and underscores only");
 			isValid = false;
 		} else {
 			document.getElementById("passwordError").innerHTML = "";
