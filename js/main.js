df7d5c893e309d6fa5e075d0d8e81973d420d65d
zacchelynmejias <zacchelynmejias@gmail.com>
2023-10-15 22:27:00 +0800

    toggling password

--- js/main.js
+++ js/main.js
@@ -13,19 +13,31 @@
 	fullHeight();
 
 
-	$(".toggle-password").click(function() {
+	// $(".toggle-password").click(function() {
 
-	  $(this).toggleClass("fa-eye fa-eye-slash");
-	  var input = $($(this).attr("toggle"));
+	//   $(this).toggleClass("fa-eye fa-eye-slash");
+	//   var input = $($(this).attr("toggle"));
 
-	  if (input.attr("type") == "password") {
-	    input.attr("type", "text");
-	  } else {
-	    input.attr("type", "password");
-	  }  
-	});
+	//   if (input.attr("type") == "password") {
+	//     input.attr("type", "text");
+	//   } else {
+	//     input.attr("type", "password");
+	//   }  
+	// });
 
 })(jQuery);
 
+function togglePasswordVisibility(icon) {
+	var passwordField = document.querySelector(icon.getAttribute('toggle'));
 
+	if (passwordField.type === "password") {
+		passwordField.type = "text";
+		icon.classList.remove("fa-eye");
+		icon.classList.add("fa-eye-slash");
+	} else {
+		passwordField.type = "password";
+		icon.classList.remove("fa-eye-slash");
+		icon.classList.add("fa-eye");
+	}
+}
 
